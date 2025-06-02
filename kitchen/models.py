9b1e58145a7efc0c5811c694ebda8bcba20109d3
Yaroslav Kalugin <yaroslav.kalugin.07@gmail.com>
2025-06-02 17:13:37 +0300

    add models to models.py and add new settings to settings.py

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,35 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.username}, ({self.first_name}, {self.last_name})"
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=8)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    cooks = models.ManyToManyField(Cook)
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+    dishes = models.ManyToManyField(Dish)
+
+    def __str__(self):
+        return self.name
